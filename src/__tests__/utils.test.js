78d467cdeb5f6dfa49079af3d5eabfe63a6d0145
Mark Brian <markbkiunga2017@gmail.com>
2024-08-09 12:39:04 +0300

    Completes Lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,25 @@
 // Your tests here
+//Passes if the function returns true for a palindrome and false if otherwise
+import { isPalindrome } from '../utils';
+
+describe('isPalindrome', () => {
+  it('returns true if the input is a palindrome and  is case-insensitive', () => {
+    //Arrange
+    const word1 = 'Racecar';
+    const word2 = 'car';
+    //Act
+    const result = isPalindrome(word1);
+    //Assert
+    expect(result).toBe(true);
+  });
+  it("throws an error if the input has any non-alphabetic characters", () => {
+    const word = "race car";
+
+    expect(() => isPalindrome(word)).toThrow();
+  });
+  it("throws an error if the input is an empty string", () => {
+    const word = "";
+
+    expect(() => isPalindrome(word)).toThrow();
+  });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
 // Your code here
+export function isPalindrome(word) {
+  if (!/^[A-Za-z]+$/.test(word)) {
+    throw new Error("Invalid input");
+  }
+  if (word === '') {
+    return false;
+  }
+  const wordInReverse = word.split('').reverse().join('');
+  if (wordInReverse.toLowerCase() == word.toLowerCase()) {
+    return true;
+  }
+}
